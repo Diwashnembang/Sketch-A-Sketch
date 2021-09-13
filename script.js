875ac94efd793846da59e4ec47a79258e3c02cb7
Diwash Nembang <nembangd0@gmail.com>
2021-09-13 21:32:06 +0545

    checkng touch and somewhat fixed the lag bug

--- script.js
+++ script.js
@@ -6,130 +6,118 @@ features user can draw in a canvas
 */
 // let j;
 // let d;
-let brush="black";
+let brush = ["black"];
 
+// move indicator dom
+const range = document.querySelector(".slider");
+const indicator = document.querySelector(".indicator-text");
 
 // move the pointer above the slider
 function moveIndicator() {
-    const range = document.querySelector(".slider");
-    const indicator = document.querySelector(".indicator-text");
-
     range.addEventListener("input", (e) => {
         indicator.textContent = range.value;
-        indicator.style.left = range.value * 1.48 + "%";
+        indicator.style.left = range.value+"%";
         // makeGrid(range.value,range.value);
     });
 }
 
+// const canvas = document.querySelector(".canvas");
 function makeGrid(width, row) {
-
-    canvas = document.querySelector(".canvas");
     canvas.setAttribute("style", `grid-template-columns:repeat(${width},1fr`);
     for (let i = 0; i < width * row; i++) {
-       
         let cell = document.createElement("div");
         cell.setAttribute("style", "border:solid 0.1px black");
         canvas.appendChild(cell);
         cell.classList.toggle("cell");
     }
-    canvas.addEventListener("mousedown", readyToDraw);
-    canvas.addEventListener("touchstart",readyToDraw);
 }
 
-
-function readyToDraw(e) {
-    const children =document.querySelector(".canvas").children;
+const children = document.querySelector(".canvas").children;
+function readyToDraw() {
+//  if (start) return console.log("nodraw out");
     const cells = Array.from(children);
-    console.log(e.type)
-
     cells.forEach((cell) => {
-            let draw=drawing.bind(cell,brush)
-            cell.onclick = () => (cell.style.background = brush);
-            // for windows /mouse
-            if(e.type==="mousedown"){
-                cell.addEventListener("mousemove",draw);
-                document.addEventListener("mouseup", (e) => { 
-                    cell.removeEventListener("mousemove", draw)
-                    
-                });
-        }
+        let draw = drawing.bind(cell);
+        // cell.onmousedown = () => draw();
+        // for windows /mouse
+        cell.addEventListener("mouseup", draw);
+        cell.addEventListener("mouseover", draw);
+        document.addEventListener("mouseup", (e) => {
+            cell.removeEventListener("mouseover", draw);
+            cell.removeEventListener("mouseup",draw);
+        });
 
-            //for mobile phones
-            cell.ontouch = () => (cell.style.background = brush);
-            cell.addEventListener("touchmove",draw);
-            document.addEventListener("touchend", (e) => { 
-                cell.removeEventListener("touchend", draw)
-
+        //for mobile phones
+        cell.addEventListener("touchend", draw);
+        cell.addEventListener("touchmove", draw);
+        document.addEventListener("touchend", (e) => {
+            cell.removeEventListener("touchend", draw);
+            cell.addEventListener("touchmove", draw);
 
             // console.log(document);
         });
-})
+    });
+
 }
 
+function drawing(color) {
+    this.style.background = brush[0];
 
-function drawing(color){
-    this.style.background=color;
+    // state.push(this);
 }
-// function noDraw(cells,d) {
-//     cells.forEach((cell) => {
-//         cell.removeEventListener("mousemove", d);
-//     });
-// }
 
-
+const pallet = document.querySelector(".pallet");
+const palletChidrens = pallet.children;
+const colors = Array.from(palletChidrens);
 function makeColorPallet() {
-    const pallet = document.querySelector(".pallet");
-    const palletChidrens = pallet.children;
-    const colors = Array.from(palletChidrens);
     colors.forEach((color) => {
         let colorName = color.getAttribute("data-color");
         color.style.background = colorName;
         // console.log(colorName,"and",color);
-        color.addEventListener("click",()=>brush=colorName);
+        color.addEventListener("click", () => (brush[0] = colorName));
     });
 }
 
-function clearButton(){
-    const clear=document.querySelector(".button");
-    clear.addEventListener("click",()=>{
-        const canvasChildren=document.querySelector(".canvas").children;
-        const cells=Array.from(canvasChildren);
+const clear = document.querySelector(".button");
+function clearButton() {
+    clear.addEventListener("click", () => {
+        const canvasChildren = document.querySelector(".canvas").children;
+        const cells = Array.from(canvasChildren);
         reset(cells);
     });
-
 }
 
-function reset(cells){
-    console.log("yes");
-    cells.forEach(cell=>{cell.style.background="#c4c4c4"})
-    
+function reset(cells) {
+    cells.forEach((cell) => {
+        cell.style.background = "#c4c4c4";
+    });
 }
-    
-function changeGrid(){
-    const range = document.querySelector(".slider");
-    
-    const canvas=document.querySelector(".canvas");
-    range.addEventListener("input",()=>{
-        while(canvas.firstChild){
+
+const slider = document.querySelector(".slider");
+
+const canvas = document.querySelector(".canvas");
+function changeGrid() {
+    slider.addEventListener("input", () => {
+        while (canvas.firstChild) {
             canvas.removeChild(canvas.firstChild);
         }
-     
-        makeGrid(range.value,range.value);
-    
-    })
 
+        makeGrid(slider.value, slider.value);
+        start=false;
+    });
 }
-
 
 
 
 function main() {
     moveIndicator();
     makeGrid(16, 16);
-    changeGrid()
+    changeGrid();
     makeColorPallet();
     clearButton();
     changeGrid();
 }
-
+let start=false
 main();
+canvas.addEventListener("mousedown", (e) => readyToDraw());
+canvas.addEventListener("touchstart", readyToDraw);
